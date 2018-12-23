eae94f839cf72de255da10fd2c6e4f8a14c65d85
abdur razzak <a.razzak660@gmail.com>
2018-12-23 09:32:44 +0600

    update

--- SQL/read_write.py
+++ SQL/read_write.py
@@ -12,11 +12,9 @@ with open('./table_list.txt') as fobj:
 for line in lines:
     table_name = line
     # print(table_name)
-    print("DELETE FROM " + table_name + "    WHERE company_code=companycode;")
+    # print("DELETE FROM " + table_name + "    WHERE company_code=companycode;")
+    print("DELETE FROM " + table_name + "    WHERE company_code='OMEGA';")
+    # print("DELETE FROM " + table_name + "    WHERE company_code='EBL';")
+    # print("DELETE FROM " + table_name + "    WHERE company_code='GLIL';")
    
 
-# ## Writing to the file data_delete.sql
-# with open('data_delete.sql', 'w') as fobj:
-#     fobj.write('Hello!')
-#     fobj.write('\n')
-#     fobj.write('How are you?')
