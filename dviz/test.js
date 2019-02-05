fc558bc8ddef5acb3b42558cc8a94ea7a3e3729b
Abdur Razzak <abdur.razzak@ctrends-software.com>
2019-02-06 00:53:47 +0600

    dviz updated

---
+++ dviz/test.js
@@ -0,0 +1,30 @@
+var styles = [
+  {
+    'background':'red',
+    'color':'white',
+    'width':'35'
+  },
+  {
+    'background':'yellolw',
+    'color':'black',
+    'width':'25'
+  },
+  {
+    'background':'green',
+    'color':'white',
+    'width':'20'
+  },
+  {
+    'background':'black',
+    'color':'white',
+    'width':'10'
+  },
+  {
+    'background':'orange',
+    'color':'white',
+    'width':'10'
+  }
+];
+
+
+console.log(styles[0].width);
